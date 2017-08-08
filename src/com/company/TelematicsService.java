1291bd511b04415d558640e5c0b046dc551c2a85
April Ross <aprilross@aprils-mbp.lan>
2017-08-08 12:20:37 -0400

    revised

--- src/com/company/TelematicsService.java
+++ src/com/company/TelematicsService.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import javax.swing.text.html.HTML;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -38,12 +35,13 @@ public class TelematicsService {
                     ObjectMapper mapper = new ObjectMapper();
                     info = mapper.readValue(f, VehicleInfo.class);
                     carInfo.add(info);
-                    System.out.println(carInfo);
+                    System.out.println(info);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+
         //Find averages
         int noOfVehicles = carInfo.size();
         double averageOdometer = 0;
@@ -64,10 +62,6 @@ public class TelematicsService {
 
         //need to adjust decimal places on average figures
 
-        System.out.println(averageOdometer);
-        System.out.println(averageConsumption);
-        System.out.println(averageLastOilChange);
-        System.out.println(averageEngineSize);
 
         //read dashboard html file into an arraylist
 //        try {
@@ -86,7 +80,7 @@ public class TelematicsService {
 //            e.printStackTrace();
 //        }
 
-       Map<Integer, String> CarHTML = new HashMap<Integer, String>();
+       Map<Integer, String> CarHTML = new HashMap<Integer,String>();
        CarHTML.put(0, "<html>");
        CarHTML.put(1, "<title>Vehicle Telematics Dashboard</title>");
        CarHTML.put(2, "<body>");
@@ -96,7 +90,7 @@ public class TelematicsService {
        CarHTML.put(6, "<th>Odometer (miles) |</th><th>Consumption (gallons) |</th><th>Last Oil Change |</th><th>Engine Size (liters)</th>");
        CarHTML.put(7, "</tr>");
        CarHTML.put(8, "<tr>\n" +
-               "            <td align=\"center\">#</td><td align=\"center\">#</td><td align=\"center\">#</td align=\"center\"><td align=\"center\">#</td>\n" +
+               "            <td align=\"center\">"+ averageOdometer + "</td><td align=\"center\">"+averageConsumption + "</td><td align=\"center\">"+ averageLastOilChange + "</td align=\"center\"><td align=\"center\">" +averageEngineSize +"</td>\n" +
                "        </tr>");
        CarHTML.put(9, "</table>\n" +
                "    <h1 align=\"center\">History</h1>\n" +
@@ -105,7 +99,7 @@ public class TelematicsService {
                "            <th>VIN</th><th>Odometer (miles)</th><th>Consumption (gallons)</th><th>Last Oil Change</th><th>Engine Size (liters)</th>\n" +
                "        </tr>\n" +
                "        <tr>\n" +
-               "            <td align=\"center\">#</td><td align=\"center\">#</td><td align=\"center\">#</td><td align=\"center\">#</td align=\"center\"><td align=\"center\">#</td>\n" +
+               "            <td align=\"center\">" + vehicleInfo.getVIN() + "/td><td align=\"center\">" +vehicleInfo.getOdometer() +"</td><td align=\"center\">" + vehicleInfo.getConsumption() + "/td><td align=\"center\">" + vehicleInfo.getOilChangeOdometer() + "</td align=\"center\"><td align=\"center\">" + vehicleInfo.getEngineSize() +"/td>\n" +
                "        </tr>\n" +
                "        <tr>\n" +
                "            <td align=\"center\">45435</td><td align=\"center\">123</td><td align=\"center\">234</td><td align=\"center\">345</td align=\"center\"><td align=\"center\">4.5</td>\n" +
@@ -113,6 +107,25 @@ public class TelematicsService {
                "    </table>");
        CarHTML.put(10, "</body>\n" +
                "</html>");
+
+       for (Integer name : CarHTML.keySet()){
+           String key = name.toString();
+           String value = CarHTML.get(name).toString();
+           System.out.println(key+ " " + value);
+       }
+
+//        FileWriter fWriter = null;
+//        BufferedWriter writer = null;
+//        try {
+//            fWriter = new FileWriter("newDashboard.html");
+//            writer = new BufferedWriter(fWriter);
+//            writer.write();
+//            writer.close(); //make sure you close the writer object
+//        } catch (Exception e) {
+//            //catch any exceptions here
+//        }
+
+
 
 
 
